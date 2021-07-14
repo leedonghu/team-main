67e82564d77eab5bbff9d40c785e612be887fc0b
leedonghu <ldh930517@gmail.com>
2021-07-15 08:50:46 +0900

    7/15

--- src/main/webapp/WEB-INF/sql/pro_board1.sql
+++ src/main/webapp/WEB-INF/sql/pro_board1.sql
@@ -1,7 +1,20 @@
 use spr1;
-DESC tbl_board;
+DESC tbl_reply;
+SELECT * from tbl_reply;
+
 
 use project;
+CREATE TABLE reply
+(
+rno INT PRIMARY KEY AUTO_INCREMENT,
+bno INT NOT NULL,
+content VARCHAR(500) not null,
+replyer VARCHAR(50) not null,
+replyDate TIMESTAMP DEFAULT now(),
+updateDate TIMESTAMP DEFAULT now(),
+FOREIGN KEY (bno) REFERENCES board (bno)
+
+);
 CREATE TABLE board
 (
 bno INT PRIMARY KEY AUTO_INCREMENT,
@@ -10,7 +23,14 @@ content VARCHAR(2000) not null,
 writer VARCHAR(50) not null,
 regdate TIMESTAMP DEFAULT now(),
 updateDate TIMESTAMP DEFAULT now(),
+cnt int DEFAULT 0,
 FOREIGN KEY (writer) REFERENCES member(userId)
 );
 DESC member;
-SELECT * from member_auth;
+SELECT * from member;
+SELECT * from board ORDER BY bno DESC;
+
+
+
+
+DROP TABLE board;
